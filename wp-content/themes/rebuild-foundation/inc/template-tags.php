c5f7e9be4f2f57943568e19ee7e4bbc676267806
Pea <pea@misfist.com>
2015-10-05 02:48:40 -0400

    Updated meta links in template tags

--- wp-content/themes/rebuild-foundation/inc/template-tags.php
+++ wp-content/themes/rebuild-foundation/inc/template-tags.php
@@ -12,31 +12,31 @@ if ( ! function_exists( 'rebuild_foundation_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function rebuild_foundation_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
+    $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+    if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
 
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
+    $time_string = sprintf( $time_string,
+        esc_attr( get_the_date( 'c' ) ),
+        esc_html( get_the_date() ),
+        esc_attr( get_the_modified_date( 'c' ) ),
+        esc_html( get_the_modified_date() )
+    );
 
-	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'rebuild-foundation' ),
-		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
-	);
+    $posted_on = sprintf(
+        esc_html_x( 'Posted on %s', 'post date', 'rebuild-foundation' ),
+        '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+    );
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'rebuild-foundation' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
+    $byline = sprintf(
+        esc_html_x( 'by %s', 'post author', 'rebuild-foundation' ),
+        '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+    );
 
-	if ( 'post' === get_post_type() ) {
-		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
-	}
+    if ( 'post' === get_post_type() ) {
+        echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+    }
 
 }
 endif;
@@ -53,84 +53,92 @@ function rebuild_foundation_entry_footer() {
     //     printf( '<span class="meta cat-links">' . esc_html__( 'Site %1$s', 'rebuild-foundation' ) . '</span>', $site_list ); // WPCS: XSS OK.
     // }
 
-    /* translators: used between list items, there is a space after the comma */
-	$site_term = wp_get_post_terms( get_the_ID(), 'rebuild_sites_category', array( 'fields' => 'slugs' ) );
-
-	if( $site_term ) {
-
-		$sites_args = array (
-			'post_type' => 'rebuild_sites',
-			'posts_per_page' => 1,
-			'tax_query' => array(
-				array(
-					'taxonomy' => 'rebuild_sites_category',
-					'field'    => 'slug',
-					'terms'    => $site_term[0]
-				),
-			),
-		);
-
-		$sites = get_posts( $sites_args );
-
-		if ( $sites ) {
-
-			$site_link = '<a href="' . post_permalink( $sites[0]->ID ) . '">' . $sites[0]->post_title . '</a>';
-			
-			printf( '<span class="meta site-links">' . esc_html__( 'Site %1$s', 'rebuild-foundation' ) . '</span>', $site_link ); // WPCS: XSS OK.
-
-		}
-
-	}
 
     /* translators: used between list items, there is a space after the comma */
-	$categories_list = get_the_category_list( esc_html__( ', ', 'rebuild-foundation' ) );
-	if ( $categories_list ) {
-		printf( '<span class="meta site-links">' . esc_html__( 'Category %1$s', 'rebuild-foundation' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-	}
+    $site_term = wp_get_post_terms( get_the_ID(), 'rebuild_sites_category', array( 'fields' => 'slugs' ) );
 
-    /* translators: used between list items, there is a space after the comma */
-    $tags_list = get_the_tag_list( '', esc_html__( ', ', 'rebuild-foundation' ) );
-    if ( $tags_list ) {
-        printf( '<span class="meta tags-links">' . esc_html__( 'Tagged %1$s', 'rebuild-foundation' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+    if( $site_term ) {
+
+        $sites_args = array (
+            'post_type' => 'rebuild_sites',
+            'posts_per_page' => 1,
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'rebuild_sites_category',
+                    'field'    => 'slug',
+                    'terms'    => $site_term[0]
+                ),
+            ),
+        );
+
+        $sites = get_posts( $sites_args );
+
+        if ( $sites ) {
+
+            // If there is a short name, use that, otherwise use full name
+            $site_name = get_post_meta( $sites[0]->ID, '_rebuild_site_short_name', true ) ? get_post_meta( $sites[0]->ID, '_rebuild_site_short_name', true ) : $sites[0]->post_title;
+
+            $site_link = '<a href="' . post_permalink( $sites[0]->ID ) . '">' . $site_name . '</a>';
+            
+            printf( '<span class="meta site-links">' . esc_html__( 'Site %1$s', 'rebuild-foundation' ) . '</span>', $site_link ); // WPCS: XSS OK.
+
+        }
+
     }
 
-    if ( 'events' === get_post_type() ) { 
-	    /* translators: used between list items, there is a space after the comma */
-	    $eventcat_list =  get_the_term_list( get_the_ID(), 'event-categories', '', ', ' );
-	    if ( $eventcat_list ) {
-	        printf( '<span class="meta eventcat-links">' . esc_html__( 'Event Category %1$s', 'rebuild-foundation' ) . '</span>', $eventcat_list ); // WPCS: XSS OK.
-	    }
+    if ( 'event' === get_post_type() ) { 
+        /* translators: used between list items, there is a space after the comma */
+        $eventcat_list =  get_the_term_list( get_the_ID(), 'event-categories', '', ', ' );
+        if ( $eventcat_list ) {
+            printf( '<span class="meta eventcat-links">' . esc_html__( 'Tagged %1$s', 'rebuild-foundation' ) . '</span>', $eventcat_list ); // WPCS: XSS OK.
+        }
+
+     } else {
+
+        /* translators: used between list items, there is a space after the comma */
+        $categories_list = get_the_category_list( esc_html__( ', ', 'rebuild-foundation' ) );
+        if ( $categories_list ) {
+            printf( '<span class="meta site-links">' . esc_html__( 'Category %1$s', 'rebuild-foundation' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+        }
+
+        /* translators: used between list items, there is a space after the comma */
+        $tags_list = get_the_tag_list( '', esc_html__( ', ', 'rebuild-foundation' ) );
+        if ( $tags_list ) {
+            printf( '<span class="meta tags-links">' . esc_html__( 'Tagged %1$s', 'rebuild-foundation' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+        }
+        
+     }
+
+    // Hide category and tag text for pages.
+    // if ( 'post' === get_post_type() ) {
+    //  /* translators: used between list items, there is a space after the comma */
+    //  $categories_list = get_the_category_list( esc_html__( ', ', 'rebuild-foundation' ) );
+    //  if ( $categories_list && rebuild_foundation_categorized_blog() ) {
+    //      printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'rebuild-foundation' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+    //  }
+
+    //  /* translators: used between list items, there is a space after the comma */
+    //  $tags_list = get_the_tag_list( '', esc_html__( ', ', 'rebuild-foundation' ) );
+    //  if ( $tags_list ) {
+    //      printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'rebuild-foundation' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+    //  }
+    // }
+
+    if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+        echo '<span class="comments-link">';
+        comments_popup_link( esc_html__( 'Leave a comment', 'rebuild-foundation' ), esc_html__( '1 Comment', 'rebuild-foundation' ), esc_html__( '% Comments', 'rebuild-foundation' ) );
+        echo '</span>';
     }
-	// Hide category and tag text for pages.
-	// if ( 'post' === get_post_type() ) {
-	// 	/* translators: used between list items, there is a space after the comma */
-	// 	$categories_list = get_the_category_list( esc_html__( ', ', 'rebuild-foundation' ) );
-	// 	if ( $categories_list && rebuild_foundation_categorized_blog() ) {
-	// 		printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'rebuild-foundation' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-	// 	}
 
-	// 	/* translators: used between list items, there is a space after the comma */
-	// 	$tags_list = get_the_tag_list( '', esc_html__( ', ', 'rebuild-foundation' ) );
-	// 	if ( $tags_list ) {
-	// 		printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'rebuild-foundation' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-	// 	}
-	// }
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		comments_popup_link( esc_html__( 'Leave a comment', 'rebuild-foundation' ), esc_html__( '1 Comment', 'rebuild-foundation' ), esc_html__( '% Comments', 'rebuild-foundation' ) );
-		echo '</span>';
-	}
-
-	edit_post_link(
-		sprintf(
-			/* translators: %s: Name of current post */
-			esc_html__( 'Edit %s', 'rebuild-foundation' ),
-			the_title( '<span class="screen-reader-text">"', '"</span>', false )
-		),
-		'<span class="edit-link">',
-		'</span>'
-	);
+    edit_post_link(
+        sprintf(
+            /* translators: %s: Name of current post */
+            esc_html__( 'Edit %s', 'rebuild-foundation' ),
+            the_title( '<span class="screen-reader-text">"', '"</span>', false )
+        ),
+        '<span class="edit-link">',
+        '</span>'
+    );
 }
 endif;
 
@@ -141,49 +149,49 @@ endif;
  */
 
 function rebuild_foundation_categorized_blog() {
-	if ( false === ( $all_the_cool_cats = get_transient( 'rebuild_foundation_categories' ) ) ) {
-		// Create an array of all the categories that are attached to posts.
-		// $all_the_cool_cats = get_categories( array(
-		// 	'fields'     => 'ids',
-		// 	'hide_empty' => 1,
+    if ( false === ( $all_the_cool_cats = get_transient( 'rebuild_foundation_categories' ) ) ) {
+        // Create an array of all the categories that are attached to posts.
+        // $all_the_cool_cats = get_categories( array(
+        //  'fields'     => 'ids',
+        //  'hide_empty' => 1,
 
-		// 	// We only need to know if there is more than one category.
-		// 	'number'     => 2,
-		// ) );
+        //  // We only need to know if there is more than one category.
+        //  'number'     => 2,
+        // ) );
 
-		wp_get_post_terms( 'rebuild_sites_category', array( 
-			'fields' => 'ids',
-			'hide_empty' => 1,
-			// We only need to know if there is more than one category.
-			'number'     => 2,
-			)
-		);
+        wp_get_post_terms( 'rebuild_sites_category', array( 
+            'fields' => 'ids',
+            'hide_empty' => 1,
+            // We only need to know if there is more than one category.
+            'number'     => 2,
+            )
+        );
 
-		// Count the number of categories that are attached to the posts.
-		$all_the_cool_cats = count( $all_the_cool_cats );
+        // Count the number of categories that are attached to the posts.
+        $all_the_cool_cats = count( $all_the_cool_cats );
 
-		set_transient( 'rebuild_foundation_categories', $all_the_cool_cats );
-	}
+        set_transient( 'rebuild_foundation_categories', $all_the_cool_cats );
+    }
 
-	// if ( $all_the_cool_cats > 1 ) {
-	// 	// This blog has more than 1 category so rebuild_foundation_categorized_blog should return true.
-	// 	return true;
-	// } else {
-	// 	// This blog has only 1 category so rebuild_foundation_categorized_blog should return false.
-	// 	return false;
-	// }
-	return true;
+    // if ( $all_the_cool_cats > 1 ) {
+    //  // This blog has more than 1 category so rebuild_foundation_categorized_blog should return true.
+    //  return true;
+    // } else {
+    //  // This blog has only 1 category so rebuild_foundation_categorized_blog should return false.
+    //  return false;
+    // }
+    return true;
 }
 
 /**
  * Flush out the transients used in rebuild_foundation_categorized_blog.
  */
 function rebuild_foundation_category_transient_flusher() {
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return;
-	}
-	// Like, beat it. Dig?
-	delete_transient( 'rebuild_foundation_categories' );
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+    // Like, beat it. Dig?
+    delete_transient( 'rebuild_foundation_categories' );
 }
 add_action( 'edit_category', 'rebuild_foundation_category_transient_flusher' );
 add_action( 'save_post',     'rebuild_foundation_category_transient_flusher' );
@@ -196,9 +204,9 @@ add_action( 'save_post',     'rebuild_foundation_category_transient_flusher' );
 
 function rebuild_foundation_get_site_link() {
 
-	$site_cat = wp_get_post_terms( $post_id, 'rebuild_sites_category', array( "fields" => "slugs" ) )[0];
+    $site_cat = wp_get_post_terms( $post_id, 'rebuild_sites_category', array( "fields" => "slugs" ) )[0];
 
-	
+    
 
 }
 
