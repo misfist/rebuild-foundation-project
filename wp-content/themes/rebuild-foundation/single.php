ea86f3aa2f4dba9dee976c003561ff29157c2b8f
Pea <pea@misfist.com>
2015-10-04 23:33:01 -0400

    Modified single to call template parts based on post type

--- wp-content/themes/rebuild-foundation/single.php
+++ wp-content/themes/rebuild-foundation/single.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+		
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<?php get_template_part( 'template-parts/content-single', get_post_type() ); ?>
