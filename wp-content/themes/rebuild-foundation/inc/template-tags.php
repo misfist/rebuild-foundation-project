e0199f1b1363e48409fb94e592995e3687df43b3
Pea <pea@misfist.com>
2015-10-17 21:00:53 -0400

    Cleaned up template tags and added new tags for displaying site category, site and location info.

--- wp-content/themes/rebuild-foundation/inc/template-tags.php
+++ wp-content/themes/rebuild-foundation/inc/template-tags.php
@@ -145,46 +145,97 @@ function rebuild_foundation_category_transient_flusher() {
 add_action( 'edit_category', 'rebuild_foundation_category_transient_flusher' );
 add_action( 'save_post',     'rebuild_foundation_category_transient_flusher' );
 
-
 /**
- * Get site full name
- * Retrieves the `post_title` of the site
- * @return string
+ * Get slug
+ * Retrieves the slug of current content
+ * @return string or @echo string
  */
 
-if(! function_exists( 'get_rebuild_site_full_name' ) ) {
+if(! function_exists( 'the_slug' ) ) {
 
-    function get_rebuild_site_full_name() {
+    function the_slug( $echo = true ) {
 
-        if( 'rebuild_sites' == get_post_type() ) {
+      $slug = basename( get_permalink() );
 
-            return get_the_title( get_the_ID() );
+      do_action( 'before_slug', $slug );
 
-        } else {
+      $slug = apply_filters( 'slug_filter', $slug );
 
-            $site_id = get_post_meta( get_the_ID(), '_rebuild_site', true );
+      echo ( $echo ) ? $slug : '';
 
-            if( isset( $site_id ) && !empty( $site_id ) && is_numeric( $site_id ) ) {
-                if( is_array( $site_id ) ) {
-                    $site_id = implode( '', $site_id );
-                } 
-                $site_info = get_post( $site_id );
+      do_action('after_slug', $slug);
 
-                var_dump( $site_id );
-
-                return $site_info->post_title;
-
-            }
-
-        }
+      return $slug;
 
     }
 
 }
 
 /**
+ * Get site category
+ * Retrieves the rebuild_site_category for current content
+ * @return string
+ */
+
+if(! function_exists( 'rebuild_get_site_category' ) ) {
+
+    function rebuild_get_site_category() {
+
+        $site_cats = get_the_terms( get_the_ID(), 'rebuild_site_category' );
+
+        if( !empty( $site_cats ) ) {
+
+            return $site_cats[0]->slug;
+
+        }
+
+        return;
+
+    }
+
+}
+
+
+/**
+ * Get site slug
+ * Retrieves the slug of the site
+ * @return string
+ */
+
+if(! function_exists( 'get_rebuild_site_slug' ) ) {
+
+    function get_rebuild_site_slug() {
+
+        if( 'rebuild_site' == get_post_type() ) {
+
+            $slug = basename( get_permalink( get_the_ID() ) );
+
+            
+        } else {
+
+            $site_cats = get_the_terms( get_the_ID(), 'rebuild_site_category' );
+
+            // Get site category slug - it should match site post slug
+            $site_cat_slug = ( !empty( $site_cats ) ) ? $site_cats[0]->slug : '';
+
+            // Get site associated with that category
+            // https://codex.wordpress.org/Function_Reference/get_page_by_path
+            $site = get_page_by_path( $site_cat_slug, OBJECT, 'rebuild_site' );
+
+            $slug = ( !empty( $site ) ) ? $site->post_name : '';
+
+        }
+
+        return $slug;
+
+    }
+
+}
+
+
+/**
  * Get site name
- * Retrieves the short name `_rebuild_site_short_name` or `post_title` of the site
+ * Retrieves the short name `short_name` or `post_title` of the site
  * @return string
  */
 
@@ -192,22 +243,34 @@ if(! function_exists( 'get_rebuild_site_name' ) ) {
 
     function get_rebuild_site_name() {
 
-        if( 'rebuild_sites' == get_post_type() ) {
-            $site_short_name = get_post_meta( get_the_ID(), '_rebuild_site_short_name', true );
-            return ( $site_short_name ) ? $site_short_name : get_the_title( get_the_ID() );
-        } else {
-            $site_id = get_post_meta( get_the_ID(), '_rebuild_site', true );
-            
-            if( isset( $site_id ) && !empty( $site_id ) && is_numeric( $site_id ) ) {
-                if( is_array( $site_id ) ) {
-                    $site_id = implode( '', $site_id );
-                } 
-                $site_info = get_post( $site_id );
-                $site_short_name = get_post_meta( $site_id, '_rebuild_site_short_name', true );
+        if( 'rebuild_site' == get_post_type() ) {
 
-                return ( $site_short_name ) ? $site_short_name : $site_info->post_title;
+            $site_short_name = get_post_meta( get_the_ID(), 'short_name', true );
+            return ( $site_short_name ) ? $site_short_name : get_the_title( get_the_ID() );
+
+        } else {
+
+            $site_cats = get_the_terms( get_the_ID(), 'rebuild_site_category' );
+
+            // If there is a site category associated with content
+            if( !empty( $site_cats ) ) {
+
+                // Get site category slug - matches site post slug
+                $site_slug = $site_cats[0]->slug;
+
+                // Get site associated with that category
+                // https://codex.wordpress.org/Function_Reference/get_page_by_path
+                $site = get_page_by_path( $site_slug, OBJECT, 'rebuild_site' );
+
+                $site_short_name = get_post_meta( $site->ID, 'short_name', true );
+
+                // If it has short name field return that
+                // Else return the_title
+                return ( $site_short_name ) ? $site_short_name : get_the_title( $site->ID );
 
             }
+
+            return;
 
         }
 
@@ -216,21 +279,92 @@ if(! function_exists( 'get_rebuild_site_name' ) ) {
 }
 
 /**
+ * Get site link
+ * Returns link to site page
+ * @return string
+ */
+
+if(! function_exists( 'rebuild_get_site_link' ) ) {
+
+    function rebuild_get_site_link() {
+
+        $site_name = get_rebuild_site_name();
+        $site_slug = get_rebuild_site_slug();
+
+        if( $site_name && $site_slug ) {
+
+            return $site_link = '<a href="/site/' . $site_slug . '">' . $site_name . '</a>';
+
+        }
+
+        return;
+
+    } 
+
+}
+
+
+/**
+ * Get site category link
+ * Renders link to site_category content relative to current content
+ * @return string
+ */
+
+if(! function_exists( 'rebuild_get_site_category_content' ) ) {
+
+    function rebuild_get_site_category_content() {
+
+        $site_name = get_rebuild_site_name();
+        $site_slug = get_rebuild_site_slug();
+
+        if( $site_name && $site_slug ) {
+
+            $post_type = get_post_type();
+            $post_type_obj = get_post_type_object( $post_type );
+
+            if( $post_type_obj->has_archive ) {
+                
+                // https://developer.wordpress.org/reference/functions/add_query_arg/
+
+                $link = '<a href="' . esc_url( add_query_arg( 'site_category', $site_slug, site_url( $post_type_obj->has_archive ) ) ) . '">' . $site_name . '</a>';
+
+            } else {
+
+                $link = '<a href="' . esc_url( add_query_arg( 'site_category', $site_slug ) ) . '">' . $site_name . '</a>';
+
+            }
+
+            echo $link;
+
+        }
+
+        return;
+
+    } 
+
+}
+
+
+/**
  * Get location information
  * Retrieves the address associated with the content
  * @return array
  */
 
-if(! function_exists( 'get_location_fields' ) ) {
-    function get_location_fields() {
+if(! function_exists( 'rebuild_get_location_fields' ) ) {
+
+    function rebuild_get_location_fields() {
 
         if( function_exists( 'get_field' ) ) {
 
-            $location = [];
-            $location_address = get_field( 'location_address' );
+            $location_id = get_field( 'location' );
 
-            if( isset( $location_address ) && is_array( $location_address ) ) {
+            $location_address = get_field( 'location_address', $location_id );
 
+            if( $location_address ) {
+                // make an array
+                $location = [];
+                
                 $address = $location_address['address'];
                 $address_fields = explode( ', ' , $address );
                 $location['address1'] = $address_fields[0];
@@ -238,71 +372,30 @@ if(! function_exists( 'get_location_fields' ) ) {
 
                 return $location;  
 
-            } else {
+            } 
 
-                // Error, no location assigned
+            return;
 
-            }
         }
     }
 }
 
-if(! function_exists( 'rebuild_get_site_link' ) ) {
+if(! function_exists( 'rebuild_formatted_address' ) ) {
 
-    function rebuild_get_site_link() {
+    function rebuild_formatted_address() {
 
-        $sites = get_the_terms( get_the_ID(), 'rebuild_sites' );
-        $permalink = ( $sites && ! is_wp_error( $sites ) )  ? get_the_permalink( $sites[0]->term_id ) : '';
-        $site_name = ( $sites && ! is_wp_error( $sites ) )  ? $sites[0]->name : '';
-        $site_link = '<a href="' . $permalink . '">' . $site_name . '</a>';
-        return ( $sites && ! is_wp_error( $sites ) ) ? $site_link : '';
+        $location = rebuild_get_location_fields();
 
-    } 
+        if( $location && is_array( $location ) ) {
 
-}
+            echo '<span class="address street">' . $location['address1'] . '</span> <span class="address city-state">' . $location['address2'] . '</span>';
 
+        }
 
-
-if(! function_exists( 'rebuild_get_location' ) ) {
-
-    function rebuild_get_location() {
-
-        $locations = get_posts( array(
-          'connected_type' => 'location_to_all',
-          'connected_items' => get_post(),
-          'nopaging' => true,
-          'suppress_filters' => false,
-          'posts_per_page' => 1
-        ) );
-
-        $location_id = ( !empty( $locations ) ) ? $locations[0]->ID : '' ;
-
-        $address = get_field( 'location_address', $location_id );
-
-        return ( !empty( $address ) ) ? get_field( 'location_address', $location_id )['address'] : '';
+        return;
 
     }
 
 }
 
-if(! function_exists( 'rebuild_get_location_name' ) ) {
 
-    function rebuild_get_location_name() {
-
-        $locations = get_posts( array(
-          'connected_type' => 'location_to_all',
-          'connected_items' => get_post(),
-          'nopaging' => true,
-          'suppress_filters' => false,
-          'posts_per_page' => 1
-        ) );
-
-        $location_id = ( !empty( $locations ) ) ? $locations[0]->ID : '' ;
-
-        $location_name = ( '' != $location_id ) ? get_the_title( $location_id ) : '';
-
-        return $location_name;
-
-    }
-
-}
