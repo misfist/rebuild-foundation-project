6c23d64e4cfd114f7118000e9e5caea4633d7d40
Remeike <r.j.b.forbes@gmail.com>
2015-10-21 19:26:37 -0400

    add font and set up masthead

--- wp-content/themes/rebuild-foundation/header.php
+++ wp-content/themes/rebuild-foundation/header.php
@@ -51,7 +51,10 @@
 		);
 	
 		wp_nav_menu( $args ); ?>
-
+	    <nav id="site-navigation" class="main-navigation" role="navigation">
+       		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'rebuild-foundation' ); ?></button>
+        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+    </nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
--- wp-content/themes/rebuild-foundation/sidebar.php
+++ wp-content/themes/rebuild-foundation/sidebar.php
@@ -11,11 +11,6 @@
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
-    <nav id="site-navigation" class="main-navigation" role="navigation">
-        <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'rebuild-foundation' ); ?></button>
-        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-    </nav><!-- #site-navigation -->
-
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) {
         dynamic_sidebar( 'sidebar-1' ); 
     }
