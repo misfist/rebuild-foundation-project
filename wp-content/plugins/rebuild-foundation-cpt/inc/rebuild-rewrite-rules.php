ce1cb1c1ede5ecf2293397987722b305233940ca
Pea <pea@misfist.com>
2015-10-24 19:46:53 -0400

    Updated rewrite rules and moved into plugin.

--- wp-content/plugins/rebuild-foundation-cpt/inc/rebuild-rewrite-rules.php
+++ wp-content/plugins/rebuild-foundation-cpt/inc/rebuild-rewrite-rules.php
@@ -3,66 +3,70 @@
 
 if(! function_exists( 'rebuild_add_rewrite_rules' ) ) {
 
-  function rebuild_add_rewrite_rules() {
+    function rebuild_add_rewrite_rules() {
 
-   /**
-    * The following set of rewrite rules states that if the URL matches this rule,
-    * http://rebuild.com/blog/sites/site
-    * http://rebuild.com/events/sites/site
-    * http://rebuild.com/exhibitions/sites/site
-    */
+        //--- Exhibitions
+        // http://rebuild.com/exhibitions/scope/{cat}/site/{site-cat}
+        // e.g. http://rebuild.site/exhibitions/scope/current/site/stony-island-arts-bank/
+        add_rewrite_rule('^exhibitions/scope/([^/]*)/site/([^/]*)/?','index.php?exhibition_category=$matches[1]&site_category=$matches[2]','top');
 
-    // Feeds
-    // add_rewrite_rule( 
-    //     '(events|blog|exhibitions)/sites/([^/]+)/feed/(feed|rdf|rss|rss2|atom)/?$', 
-    //     'index.php?post_type=rebuild_$matches[1]&rebuild_site=$matches[2]&feed=$matches[3]', 
-    //     'top' );
+        // http://rebuild.com/exhibitions/site/{site-cat}/scope/{cat}
+        // e.g. http://rebuild.site/exhibitions/site/stony-island-arts-bank/scope/current/
+        add_rewrite_rule('^exhibitions/site/([^/]*)/scope/([^/]*)/?','index.php?exhibition_category=$matches[2]&site_category=$matches[1]','top');
 
-    // add_rewrite_rule( 
-    //     '(events|blog|exhibitions)/sites/([^/]+)/(feed|rdf|rss|rss2|atom)/?$', 
-    //     'index.php?post_type=rebuild_$matches[1]&rebuild_site=$matches[2]&feed=$matches[2]', 
-    //     'top' ); 
+        // http://rebuild.com/exhibitions/scope/{cat}
+        // e.g. http://rebuild.site/exhibitions/scope/current/
+        add_rewrite_rule('^exhibitions/scope/([^/]*)/?','index.php?exhibition_category=$matches[1]','top');
 
-    // // Pagination
-    // add_rewrite_rule( 
-    //     '(events|blog|exhibitions)/sites/([^/]+)/page/?([0-9]{1,})/?$', 
-    //     'index.php?post_type=rebuild_$matches[1]&rebuild_site=$matches[2]&paged=$matches[3]', 
-    //     'top' );
+        //--- Events
+        // e.g. http://rebuild.com/events/type/{cat}/site/{site}
+        add_rewrite_rule('^events/type/([^/]*)/site/([^/]*)/?','index.php?event_category=$matches[1]&site_category=$matches[2]','top');
 
-    // Base rewrite 
-     //rebuild.site/{{post_type}}/{{site_slug}}/
-    // add_rewrite_rule( 
-    //     'events/?$', 
-    //     'index.php?post_type=rebuild_event', 
-    //     'top' ); 
-    // add_rewrite_rule( 
-    //     'exhibitions/?$', 
-    //     'index.php?post_type=rebuild_exhibition', 
-    //     'top' ); 
-    // add_rewrite_rule( 
-    //     'locations/?$', 
-    //     'index.php?post_type=rebuild_location', 
-    //     'top' );
-    // add_rewrite_rule( 
-    //     'sites/?$', 
-    //     'index.php?taxonomy=rebuild_site_category', 
-    //     'top' ); 
+        // e.g. http://rebuild.com/events/site/{site}/type/{cat}
+        add_rewrite_rule('^events/site/([^/]*)/type/([^/]*)/?','index.php?event_category=$matches[2]&site_category=$matches[1]','top');
 
-    // Post type by site and category
-    //rebuild.site/{{post_type}}/sites/{{site-slug}}/category/{{category-slug}}/
-    // add_rewrite_rule( 
-    //     '(events|blog|exhibitions)/sites/([^/]+)/category/([^/]+)/?$', 
-    //     'index.php?post_type=rebuild_$matches[1]&rebuild_site=$matches[2]&rebuild_$matches[1]_category=$matches[3]', 
-    //     'top' ); 
+        // e.g. http://rebuild.com/events/tag/{cat}/site/{site}
+        add_rewrite_rule('^events/tag/([^/]*)/site/([^/]*)/?','index.php?event_tag=$matches[1]&site_category=$matches[2]','top');
 
-    // // Post type by site
-    // add_rewrite_rule( '(events|blog|exhibitions)/sites/([^/]+)/?$', 
-    //     'index.php?post_type=rebuild_$matches[1]&rebuild_site=$matches[2]', 
-    //     'top' );
+        // e.g. http://rebuild.com/events/site/{site}/tag/{cat}
+        add_rewrite_rule('^events/site/([^/]*)/tag/([^/]*)/?','index.php?event_tag=$matches[2]&site_category=$matches[1]','top');
 
-  }
+        // e.g. http://rebuild.com/events/site/{site}
+        add_rewrite_rule('^events/site/([^/]*)/?','index.php?post_type=rebuild_event&site_category=$matches[1]','top');
 
-  add_action( 'init', 'rebuild_add_rewrite_rules' );
+        // e.g. http://rebuild.com/events/type/{cat}
+        add_rewrite_rule('^events/type/([^/]*)/?','index.php?event_category=$matches[1]','top');
+
+        // e.g. http://rebuild.com/events/tag/{tag}
+        add_rewrite_rule('^events/tag/([^/]*)/?','index.php?event_tag=$matches[1]','top');
+
+        //--- Posts
+
+        // e.g. http://rebuild.com/blog/site/{site}/tag/{tag}
+        add_rewrite_rule('^blog/site/([^/]*)/tag/([^/]*)/?','index.php?post_type=post&site_category=$matches[1]&post_tag=$matches[2]','top');
+
+        // e.g. http://rebuild.com/blog/site/{site}/category/{cat}
+        add_rewrite_rule('^blog/site/([^/]*)/category/([^/]*)/?','index.php?post_type=post&site_category=$matches[1]&category=$matches[2]','top');
+
+        // e.g. http://rebuild.com/blog/tag/{tag}/site/{site}
+        add_rewrite_rule('^blog/tag/([^/]*)/site/([^/]*)/?','index.php?post_type=post&site_category=$matches[2]&post_tag=$matches[1]','top');
+
+        // e.g. http://rebuild.com/blog/category/{cat}/site/{site}
+        add_rewrite_rule('^blog/category/([^/]*)/site/([^/]*)/?','index.php?post_type=post&site_category=$matches[2]&category=$matches[1]','top');
+
+        // e.g. http://rebuild.com/blog/site/{site}
+        add_rewrite_rule('^blog/site/([^/]*)/?','index.php?post_type=post&site_category=$matches[1]','top');
+
+        // e.g. http://rebuild.com/blog/tag/{tag}
+        add_rewrite_rule('^blog/tag/([^/]*)/?','index.php?post_type=post&post_tag=$matches[1]','top');
+
+        // e.g. http://rebuild.com/blog/category/{tag}
+        add_rewrite_rule('^blog/category/([^/]*)/?','index.php?post_type=post&category=$matches[1]','top');
+
+
+    }
+
+    add_action( 'init', 'rebuild_add_rewrite_rules' );
 
 }
 
