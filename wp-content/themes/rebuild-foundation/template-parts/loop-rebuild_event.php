0b70962e795bac1192010b6325147f9ea34c889e
Remeike <r.j.b.forbes@gmail.com>
2015-10-24 19:57:40 -0400

    sites listing page and events listing page

--- wp-content/themes/rebuild-foundation/template-parts/loop-rebuild_event.php
+++ wp-content/themes/rebuild-foundation/template-parts/loop-rebuild_event.php
@@ -17,10 +17,10 @@
 ?>
 
 <article id="event-<?php the_ID(); ?>" data-year="<?php echo date( 'Y', strtotime( $start_date ) ); ?>" data-month="<?php echo date( 'm', strtotime( $start_date ) ); ?>"  <?php post_class( 'event' ); ?>>
+	
+	<div class="site-name"><?php rebuild_get_site_category_content(); ?></div>
 
     <?php the_title( sprintf( '<h3 class="entry-title event-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
-
-    <div class="site-name"><?php rebuild_get_site_category_content(); ?></div>
 
     <div class="entry-meta event-date"  style="background-image: url( <?php echo $featured_image[0]; ?> )">
         <time datetime="<?php echo date( 'Y-m-d', strtotime( $start_date ) ); ?>"><?php echo date( 'M j', strtotime( $start_date ) ); ?></time>
