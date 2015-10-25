327749df6ae58a01cb91bd040e019c522504b69a
Pea <pea@misfist.com>
2015-10-25 01:56:01 -0400

    Moved shortcode to custom plugin

--- wp-content/themes/rebuild-foundation/inc/extras.php
+++ wp-content/themes/rebuild-foundation/inc/extras.php
@@ -107,59 +107,6 @@ if ( ! function_exists( 'rebuild_foundation_register_widget_area' ) ) {
 
   add_action( 'widgets_init', 'rebuild_foundation_register_widget_area' );
 
-
-
-}
-
-// Enable shortcodes in widget
-add_filter( 'widget_text', 'do_shortcode' );
-
-/**
- * Current year shortcode
- */
-
-if(! function_exists( 'rebuild_current_year' ) ) {
-
-  function rebuild_current_year() {
-
-    return date( 'Y' );
-
-  }
-
-  add_shortcode( 'year', 'rebuild_current_year' );
-
-}
-
-/**
- * Site name shortcode
- */
-
-if(! function_exists( 'rebuild_site_name' ) ) {
-
-  function rebuild_site_name() {
-
-    return get_bloginfo( 'name' );
-
-  }
-
-  add_shortcode( 'sitename', 'rebuild_site_name' );
-
-}
-
-/**
- * Site link shortcode
- */
-
-if(! function_exists( 'rebuild_site_link' ) ) {
-
-  function rebuild_site_link() {
-
-    return '<a href="' . esc_url( home_url( '/' ) ) . '" rel="home">' . get_bloginfo( 'name' ) . '</a>';
-
-  }
-
-  add_shortcode( 'sitelink', 'rebuild_site_link' );
-
 }
 
 
