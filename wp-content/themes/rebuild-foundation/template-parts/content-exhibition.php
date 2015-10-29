3269f4f145400f69cb7948fa3d11066e70b66bfa
Remeike Forbes <r.j.b.forbes@gmail.com>
2015-10-29 17:07:07 -0400

    Add en dash been start date and end date

--- wp-content/themes/rebuild-foundation/template-parts/content-exhibition.php
+++ wp-content/themes/rebuild-foundation/template-parts/content-exhibition.php
@@ -31,7 +31,7 @@
 
             <?php else: ?>
 
-                <span class="start-date"><?php echo ( $start_date ) ? date( 'F j, Y', strtotime( $start_date ) )  : ''; ?></span>
+                <span class="start-date"><?php echo ( $start_date ) ? date( 'F j, Y', strtotime( $start_date ) )  : ''; ?></span> –
                 <span class="end-date"><?php echo ( $end_date ) ? date( 'F j, Y', strtotime( $end_date ) )  : ''; ?></span>
 
             <?php endif; ?>
