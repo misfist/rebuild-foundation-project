8423dec2003dbec3ed6f1cca694d108a5082bbed
Remeike Forbes <r.j.b.forbes@gmail.com>
2015-11-02 17:12:17 -0500

    CSS for mobile

--- wp-content/themes/rebuild-foundation/header.php
+++ wp-content/themes/rebuild-foundation/header.php
@@ -55,8 +55,8 @@
 
 		
 	    <nav id="site-navigation" class="main-navigation" role="navigation">
-       		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'rebuild-foundation' ); ?></button>
-        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'depth' => 1 ) ); ?>
+       		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( '', 'rebuild-foundation' ); ?></button>
+        <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'depth' => 3 ) ); ?>
     </nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
