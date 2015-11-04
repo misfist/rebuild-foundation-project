ac69e15bee0810867c947f2e7fd80fb82fdd65ed
Pea <pea@misfist.com>
2015-11-03 19:39:24 -0500

    Added get page type helper function.

--- wp-content/themes/rebuild-foundation/inc/helpers.php
+++ wp-content/themes/rebuild-foundation/inc/helpers.php
@@ -172,5 +172,54 @@ if(! function_exists( 'rebuild_get_site_category' ) ) {
 
 }
 
+/**
+ * Get Page Type
+ * Retrieves page type of current content
+ * @return string
+ */
+
+if(! function_exists( 'rebuild_get_page_type' ) ) {
+
+    function rebuild_get_page_type() {
+
+        global $wp_query;
+        
+        $loop = 'notfound';
+
+        if ( $wp_query->is_404 ) {
+            $loop = 'notfound';
+        } elseif ( $wp_query->is_page ) {
+            $loop = is_front_page() ? 'front' : 'page';
+        } elseif ( $wp_query->is_home ) {
+            $loop = 'home';
+        } elseif ( $wp_query->is_single ) {
+            $loop = ( $wp_query->is_attachment ) ? 'attachment' : 'single';
+        } elseif ( $wp_query->is_category ) {
+            $loop = 'category';
+        } elseif ( $wp_query->is_tag ) {
+            $loop = 'tag';
+        } elseif ( $wp_query->is_tax ) {
+            $loop = 'tax';
+        } elseif ( $wp_query->is_archive ) {
+            if ( $wp_query->is_day ) {
+                $loop = 'day';
+            } elseif ( $wp_query->is_month ) {
+                $loop = 'month';
+            } elseif ( $wp_query->is_year ) {
+                $loop = 'year';
+            } elseif ( $wp_query->is_author ) {
+                $loop = 'author';
+            } else {
+                $loop = 'archive';
+            }
+        } elseif ( $wp_query->is_search ) {
+            $loop = 'search';
+        } 
+
+        return $loop;
+    }
+
+}
+
 
 ?>
