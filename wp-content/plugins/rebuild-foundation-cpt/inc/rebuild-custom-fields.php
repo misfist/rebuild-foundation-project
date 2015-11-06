9f4f5e9866b816532d3ee2a5ad575db97c893901
Pea <pea@misfist.com>
2015-11-06 13:46:32 -0500

    Made show_blog_posts not required, but still defaulted to true.

--- wp-content/plugins/rebuild-foundation-cpt/inc/rebuild-custom-fields.php
+++ wp-content/plugins/rebuild-foundation-cpt/inc/rebuild-custom-fields.php
@@ -568,7 +568,7 @@ if( function_exists( 'acf_add_local_field_group' ) ) {
                 'name' => 'show_blog_posts',
                 'type' => 'true_false',
                 'instructions' => '',
-                'required' => 1,
+                'required' => 0,
                 'conditional_logic' => 0,
                 'wrapper' => array(
                     'width' => '',
