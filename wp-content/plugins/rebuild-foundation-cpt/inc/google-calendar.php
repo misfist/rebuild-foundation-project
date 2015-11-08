36cdba5a3e2fe59c0da79916709967bfddc89ef5
Pea <pea@misfist.com>
2015-11-08 14:42:25 -0500

    Trying to fix location name display that's only an issue on dev and prod.

--- wp-content/plugins/rebuild-foundation-cpt/inc/google-calendar.php
+++ wp-content/plugins/rebuild-foundation-cpt/inc/google-calendar.php
@@ -39,7 +39,7 @@ if(! function_exists( 'rebuild_google_calendar_link' ) ) {
         $start = strtotime( $start_date . ' ' . $start_time );
         $end = strtotime( $end_date . ' ' . $end_time );
 
-        $location_id = get_field( 'location', false, false );
+        $location_id = get_field( 'location', $event_id, false );
         $location_name = get_the_title( $location_id );
         $location_address = rebuild_convert_location_to_string( $event_id );
         $event_location = $location_name . ', ' . $location_address;
