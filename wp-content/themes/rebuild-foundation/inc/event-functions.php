702869fcadc6b371b3fa8a008c5bbaad9d8917f1
Pea <pea@misfist.com>
2015-11-08 18:49:28 -0500

    Fixed year and month display so they only show if events exist for them, taking into account all query vars Added event count function and template tag

--- wp-content/themes/rebuild-foundation/inc/event-functions.php
+++ wp-content/themes/rebuild-foundation/inc/event-functions.php
@@ -9,7 +9,7 @@
 
 /**
  * Event Meta Query Vars
- * Gets the date query vars based on conditions
+ * Builds the date query vars based on conditions
  * @return array
  */
 
@@ -161,6 +161,256 @@ if(! function_exists( 'rebuild_events_pre_query_filter' ) ) {
   }
 
   add_action( 'pre_get_posts', 'rebuild_events_pre_query_filter', 10, 1 );
+
+}
+
+
+
+/**
+ * Get all the event start_dates
+ * Based on current query, creates array of dates that have posts
+ * This can be used to dynamically generate date filters
+ * @return array of dates
+ */
+
+if(! function_exists( 'rebuild_get_dates' ) ) {
+
+  function rebuild_get_dates() {
+
+    global $wp_query;
+
+    // Get the query
+    $query = ( isset( $wp_query->query ) ) ? $wp_query->query : '';
+    $post_type = ( isset( $query ) && array_key_exists( 'post_type' , $query ) ) ? $query['post_type'] : 'event';
+
+    // Get the taxonomy
+    switch ( true ) {
+
+      case ( array_key_exists( 'site_category' , $query ) ) :
+        $event_taxonomy = 'site_category';
+        $event_term = $query['site_category'];
+        break;
+
+      case ( array_key_exists( 'event_category' , $query ) ) :
+        $event_taxonomy = 'event_category';
+        $event_term = $query['event_category'];
+        break;
+
+      case ( array_key_exists( 'event_tag' , $query ) ) :
+        $event_taxonomy = 'event_tag';
+        $event_term = $query['event_tag'];
+        break;
+
+      default :
+        $event_taxonomy = null;
+        $event_term = null;
+
+    }
+
+    // Base args
+    $args = array(
+        'post_type' => $post_type,
+        'posts_per_page' => -1
+    );
+
+    // Tax args
+    if( $event_taxonomy ) {
+
+      $args['tax_query'] = array(
+          array(
+              'taxonomy' => $event_taxonomy,
+              'field'    => 'slug',
+              'terms'    => $event_term,
+          ),
+      );
+
+    }
+
+    // Get all the posts matching args
+    $events = get_posts( $args );
+
+    $dates = [];
+
+    if( count( $events ) > 0 ) {
+
+      // Populate array with dates that have posts
+      foreach( $events as $event ) {
+
+        if( get_post_meta( $event->ID, 'start_date', true ) ) {
+
+          array_push( $dates , get_post_meta( $event->ID, 'start_date', true ) );
+
+        }
+
+      }
+
+    }
+
+    return $dates;
+
+  }
+
+}
+
+/**
+ * Get Event Post Count
+ * Based on current query, returns number of matched events
+ * @return integer
+ */
+
+if(! function_exists( 'rebuild_count_events' ) ) {
+
+  function rebuild_count_events() {
+
+    global $wp_query;
+
+    // Get the query
+    $query = ( isset( $wp_query->query ) ) ? $wp_query->query : '';
+    $post_type = ( isset( $query ) && array_key_exists( 'post_type' , $query ) ) ? $query['post_type'] : 'event';
+    $event_year = ( isset( $query ) && array_key_exists( 'event_year' , $query ) ) ? $query['event_year'] : '';
+    $event_month = ( isset( $query ) && array_key_exists( 'event_month' , $query ) ) ? $query['event_month'] : '';
+
+    // Get the taxonomy
+    switch ( true ) {
+
+      case ( array_key_exists( 'site_category' , $query ) ) :
+        $event_taxonomy = 'site_category';
+        $event_term = $query['site_category'];
+        break;
+
+      case ( array_key_exists( 'event_category' , $query ) ) :
+        $event_taxonomy = 'event_category';
+        $event_term = $query['event_category'];
+        break;
+
+      case ( array_key_exists( 'event_tag' , $query ) ) :
+        $event_taxonomy = 'event_tag';
+        $event_term = $query['event_tag'];
+        break;
+
+      default :
+        $event_taxonomy = null;
+        $event_term = null;
+
+    }
+
+    // Base args
+    $args = array(
+        'post_type' => $post_type,
+        'posts_per_page' => -1
+    );
+
+    // Tax args
+    if( $event_taxonomy ) {
+
+      $args['tax_query'] = array(
+          array(
+              'taxonomy' => $event_taxonomy,
+              'field'    => 'slug',
+              'terms'    => $event_term,
+          ),
+      );
+
+    }
+
+    // If Year
+    if( $event_year || $event_month ) {
+
+      $args['meta_query'] = rebuild_events_meta_query_vars();
+
+    }
+
+    // Get all the posts matching args
+    $events = get_posts( $args );
+
+    return count( $events );
+
+  }
+
+}
+
+
+/**
+ * Get all the event years
+ * Based on the current query (ala rebuild_get_dates()) get all the years that have posts
+ * Use by rebuild_event_year_filter()
+ * @return array of year integers
+ */
+
+if(! function_exists( 'rebuild_get_event_years' ) ) {
+
+  function rebuild_get_event_years() {
+
+    if( function_exists( 'rebuild_get_dates' ) ) {
+
+      $dates = rebuild_get_dates();
+
+      return array_unique( array_map( 'convert_date_to_year', $dates ) );
+
+    }
+
+  }
+
+}
+
+
+/**
+ * Get dates by year
+ * Used by rebuild_event_month_filter()
+ * @return array
+ */
+
+if(! function_exists( 'get_dates_by_year' ) ) {
+
+  function get_dates_by_year() {
+
+    $years = get_unique_years();
+    $dates = rebuild_get_dates();
+    $date_array = [];
+
+    rsort( $date_array );
+
+    for ( $i = 0; $i < count( $years ); $i++ ) {
+
+      for ( $j = 0; $j < count( $dates ); $j++ ) {
+        
+        if ( is_date_in_year( $dates[$j], $years[$i] ) ) {
+
+          $date_array[$years[$i]][] =  date( 'm', strtotime( $dates[$j] ) );
+
+        }
+
+      }
+
+      $date_array[$years[$i]] = array_unique( $date_array[$years[$i]] );
+    
+    }
+
+    return $date_array;
+
+  }
+
+}
+
+
+/**
+ * Get unique years
+ * @return array
+ */
+
+if(! function_exists( 'get_unique_years' ) ) {
+
+  function get_unique_years() {
+
+    $dates = rebuild_get_dates();
+
+    $years = array_unique( array_map( 'convert_date_to_year', $dates ) );
+
+    rsort( $years );
+
+    return $years;
+
+  }
 
 }
 
