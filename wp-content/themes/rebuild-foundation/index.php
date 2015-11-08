44bcde5c2658c10715ff59f1ead6a24567352e34
Remeike <r.j.b.forbes@gmail.com>
2015-11-08 11:38:21 -0500

    Make header structure the same for all listing pages

--- wp-content/themes/rebuild-foundation/index.php
+++ wp-content/themes/rebuild-foundation/index.php
@@ -16,18 +16,19 @@ get_header(); ?>
 
 
 	<div id="primary" class="content-area">
+	<?php if ( have_posts() ) : ?>
+
+		<?php if ( is_home() && ! is_front_page() ) : ?>
+			<header class="page-header">
+				<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+
+				<?php rebuild_site_context_nav(); ?>
+			</header>
+		<?php endif; ?>
 
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
-
-			<?php if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-
-					<?php rebuild_site_context_nav(); ?>
-				</header>
-			<?php endif; ?>
+		
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
