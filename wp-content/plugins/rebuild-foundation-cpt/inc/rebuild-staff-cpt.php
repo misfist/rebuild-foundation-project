abfc5ea17de0f460998a7b5f6019cb18a51f0742
Pea Lutz <pea@glocal.coop>
2016-03-17 17:06:33 -0400

    Changed 'has_archive' to false so that going to /staff resolves to http://rebuild.site/about/staff/

--- wp-content/plugins/rebuild-foundation-cpt/inc/rebuild-staff-cpt.php
+++ wp-content/plugins/rebuild-foundation-cpt/inc/rebuild-staff-cpt.php
@@ -54,7 +54,7 @@ if ( ! function_exists('rebuild_foundation_staff_post_type') ) {
             'show_in_admin_bar'     => true,
             'show_in_nav_menus'     => true,
             'can_export'            => true,
-            'has_archive'           => true,        
+            'has_archive'           => false,        
             'exclude_from_search'   => false,
             'publicly_queryable'    => true,
             'capability_type'       => 'post',
